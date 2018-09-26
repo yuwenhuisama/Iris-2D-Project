212fdf87b257d9cf8542ce5d40e7db081eefb5f0
Huisama <1026121287@qq.com>
2018-09-26 19:55:22 +0800

    Update Configure Files for Visual Studio Project.

--- vs_configure_files/dependencies.bat
+++ vs_configure_files/dependencies.bat
@@ -2,5 +2,5 @@ vcpkg.exe install glew:x86-windows glew:x64-windows
 vcpkg.exe install glfw3:x86-windows glfw3:x64-windows
 vcpkg.exe install directxtex:x86-windows directxtex:x64-windows
 vcpkg.exe install glm:x86-windows glm:x64-windows
-vcpkg.exe install freetype
+vcpkg.exe install freetype:x86-windows freetype:x64-windows
 vcpkg.exe install boost:x86-windows boost:x64-windows
